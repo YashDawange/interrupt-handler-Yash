72ac963c3143d35d83879c743994df6e10db62a5
nikshayyadav23cs284 <nikshayyadav_23cs284@dtu.ac.in>
2025-12-21 11:30:30 +0530

    added some more words into backchannel words
    added a lot of short filler & continuers , affirmations and agreement , casual acknowledgements, phrases & feedback, reactions words into filler words list.

--- examples/voice_agents/basic_agent.py
+++ examples/voice_agents/basic_agent.py
@@ -33,23 +33,25 @@ load_dotenv()
 #   BACKCHANNEL_WORDS="yeah, ok, okay, hmm, uh-huh"
 #   INTERRUPT_WORDS="stop, wait, no, cancel"
 _DEFAULT_BACKCHANNEL_WORDS = {
-    "yeah",
-    "yea",
-    "yep",
-    "ok",
-    "okay",
-    "k",
-    "hmm",
-    "uh",
-    "uhh",
-    "uh-huh",
-    "mm",
-    "mm-hmm",
-    "right",
-    "sure",
-    "aha",
-    "got it", 
-    "correct"
+    # Short Fillers & Continuers
+    "ah", "aha", "hm", "hmm", "mhm", "mhmm",
+    "mm-hmm", "mmhmm", "uh-huh", "um", "uh", "uhhuh",
+
+    # Affirmations & Agreement
+    "absolutely", "exactly", "indeed", "right", "sure",
+    "true", "understood", "correct", "definitely",
+
+    # Casual Acknowledgments
+    "alright", "cool", "fine", "nice", "ok", "okay",
+    "yeah", "yep", "yes", "yup", "sounds good",
+
+    # Phrases & Feedback
+    "go on", "got it", "i see", "makes sense",
+    "keep going", "tell me more",
+
+    # Reactions
+    "really", "wow", "for real", "seriously",
+    "interesting", "no way",
 }
 
 _DEFAULT_INTERRUPT_WORDS = {
