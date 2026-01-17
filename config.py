1421e67760ec9b53d62281d061db29f1512522b3
lavishv999-rgb <lavishv999@gmail.com>
2026-01-17 12:34:23 +0530

    add config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+IGNORE_WORDS = [
+    "yeah",
+    "ok", 
+    "okay",
+    "hmm",
+    "uh-huh",
+    "right",
+    "aha",
+    "mhmm",
+    "yep",
+    "yup",
+    "sure",
+    "gotcha",
+    "alright",
+    "cool",
+    "nice"
+]
+
+DEBUG_MODE = True
+
+STT_CONFIDENCE_THRESHOLD = 0.5
